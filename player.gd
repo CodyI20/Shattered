09053fa2d5bc32f2e9f5fb2a133dccad02639fd4
Áron Gyula Forró <arongforro@gmail.com>
2024-09-20 15:17:00 +0200

    Camera bobbing

--- player.gd
+++ player.gd
@@ -1,17 +1,27 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
+
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
 var gravity = 9.8
 
 @onready var head = $head
 @onready var camera = $head/Camera3D
 
+
+var initial_camera_y = 0.0
+
 func _ready():
+	# Capture the mouse
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+	initial_camera_y = camera.transform.origin.y
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -19,9 +29,8 @@ func _unhandled_input(event):
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -29,8 +38,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get input direction and handle movement.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -39,5 +47,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
+	
+	t_bob += delta * velocity.length() * float(is_on_floor())
+
+	var camera_pos = camera.transform.origin
+	camera_pos.y = initial_camera_y + _headbob(t_bob).y
+	camera.transform.origin = camera_pos
 
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	return pos
