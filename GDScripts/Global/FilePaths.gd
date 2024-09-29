00d2eecf5c350642aaebef7a8ec7008525449f7a
Cody <codrin.ilie10@gmail.com>
2024-09-29 15:47:59 +0200

    Fixes and minor updates
     - Set the player's process to Pausable so it can properly behave when the game is paused
     - Created a new folder for global scripts and populated it with two for now (Events and FilePaths)
     - Changed the file paths in certain scripts (game_manager.gd and main_menu.gd), to the constants from the FilePaths script, to allow for a more dynamic approach and make it less error-prone
     - Cleaned up the directory by rearranging certain files and deleting unnecessary ones

---
+++ GDScripts/Global/FilePaths.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const MENU_SCENE_PATH := "res://Scenes/UIScenes/MainMenu.tscn"
+const GAME_SCENE_PATH := "res://Scenes/MainScenes (3D)/Game.tscn"
+const OPTIONS_MENU_PATH := null
--- GDScripts/Others/Menus/main_menu.gd
+++ GDScripts/Others/Menus/main_menu.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+	get_tree().change_scene_to_file(FilePaths.GAME_SCENE_PATH)
 	
 	
 func _on_quit_button_pressed() -> void:
