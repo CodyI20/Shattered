8171bcfe49590ad5c28d69cf1d324dc635cf1271
Glenn <darthvaderpie@gmail.com>
2024-10-16 16:47:21 +0200

    Working interaction system
    Made an interaction scripts that handles the interaction logic.
    Made interactable script that makes an object interactable.
    Cleaned up the filesystem a bit.
    Added a box that you can interact with in the TestLevel Scene
    Added interaction raycast to Player Scene

---
+++ GDScripts/Player/Interaction System/Interaction.gd
@@ -0,0 +1,39 @@
+extends RayCast3D
+
+var current_collider
+# Grabs the interaction label node
+@onready var interaction_label = get_node("/root/Game/UI/InteractionLabel")
+
+# This script checks if the RayCast is interacting with an interactable object and provides interactibility
+
+func _ready() -> void:
+	set_interaction_text("")
+
+
+func _process(_delta):
+	var collider = get_collider()
+# Checks if the raycast is colliding with an interactable object
+	if is_colliding() and collider is Interactable:
+# Checks if it's a different interactable object than the one it already has
+		if current_collider != collider:
+			set_interaction_text(collider.get_interaction_text())
+			current_collider = collider
+# When the interact button is pressed, will interact with object
+		if Input.is_action_just_pressed("interact"):
+			set_interaction_text(collider.get_interaction_text())
+			collider.interact()
+# If there is no object to interact with, sets the collider to nothing
+	elif current_collider:
+		current_collider = null
+		set_interaction_text("")
+
+func set_interaction_text(text):
+# Shows the interaction text when raycast is colliding with an interactable object
+	if !text:
+		interaction_label.set_text("")
+		interaction_label.set_visible(false)
+	else:
+# Grabs the interact key from the interact keymap
+# TODO: Try this fucking code and see if it works: https://docs.godotengine.org/en/stable/classes/class_inputmap.html#class-inputmap-method-action-get-events Array[InputEvent] action_get_events(action: StringName) String as_text() const
+		interaction_label.text = "Press E to interact"
+		interaction_label.visible = true
