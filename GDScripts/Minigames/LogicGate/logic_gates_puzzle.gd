818be3883c6686ed0f01f9c9efc0763308bfb07a
Cody <codrin.ilie10@gmail.com>
2024-11-02 13:34:09 +0100

    Logic gate puzzle UI update
     - Added a fix to not being able to interact with the puzzle due to layering

--- GDScripts/Minigames/LogicGate/logic_gates_puzzle.gd
+++ GDScripts/Minigames/LogicGate/logic_gates_puzzle.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var puzzle_active : bool = false
+@onready var pausable_ui: CanvasLayer = $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +13,9 @@ func toggle_puzzle() -> void:
 	visible = puzzle_active
 	set_process(puzzle_active)
 	if puzzle_active:
+		pausable_ui.layer = 10
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	else:
+		pausable_ui.layer = 0
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		
