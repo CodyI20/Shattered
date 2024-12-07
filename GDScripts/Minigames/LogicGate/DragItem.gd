f04f45de3211bb9233d88397b1925d5dd55c8d84
Codrin Ilie <codrin.ilie10@gmail.com>
2024-12-07 20:54:40 +0100

    Logic gates puzzle Update
     - Created the HARD layout

--- GDScripts/Minigames/LogicGate/DragItem.gd
+++ GDScripts/Minigames/LogicGate/DragItem.gd
@@ -9,8 +9,8 @@ var personal_zone : DropZone
 @export var gatetype = Enums.gate_type.NONE
 
 func _ready() -> void:
-	original_position = global_position
 	personal_zone = DropZone.new()
+	original_position = global_position
 	create_drop_zone()
 	Events.valid_drop_target_entered.connect(_on_valid_drop_target_entered)
 	Events.valid_drop_target_exited.connect(_on_valid_drop_target_exited)
@@ -19,18 +19,14 @@ func _ready() -> void:
 	Events.logic_gates_puzzle_layout_change.connect(reset_puzzle)
 
 func reset_puzzle() -> void:
-	if get_parent().visible == false:
-		return
 	print_debug("DragItem has been reset...")
 	global_position = original_position
 	personal_zone = DropZone.new()
 	create_drop_zone()
 	
 func create_drop_zone() -> void:
-	if get_parent().visible == false:
-		return
 	if current_slot == null:
-		personal_zone.global_position = global_position
+		personal_zone.global_position = global_position - size / 2
 		personal_zone.correct_gate = Enums.gate_type.NONE
 		add_sibling.call_deferred(personal_zone)
 		current_slot = personal_zone
@@ -58,28 +54,18 @@ func _input(event):
 
 # Dragging logic
 func _process(delta):
-	if get_parent().visible == false:
-		return
 	if dragging:
-		global_position = get_global_mouse_position() - size * 0.5  # Center the item on the cursor
+		global_position = get_global_mouse_position() - size * 0.75  # Center the item on the cursor
 
 func set_valid_drop_target(target: DropZone):
-	if get_parent().visible == false:
-		return
 	valid_drop_target = target
 
 # Slot signals handling
 func _on_valid_drop_target_entered(dropTarget: DropZone):
-	if get_parent().visible == false:
-		return
 	set_valid_drop_target(dropTarget)
 
 func _on_valid_drop_target_exited(dropTarget: DropZone):
-	if get_parent().visible == false:
-		return
 	set_valid_drop_target(null)
 	
 func disable_process() -> void:
-	if get_parent().visible == false:
-		return
 	self.set_script(null)
